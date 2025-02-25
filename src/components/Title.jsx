df6dd17a8873903aea463ff8a63a12d0cd7ddd6c
RBocaling <rbocaling@qmdsi.com>
2025-02-25 20:13:56 +0800

    upload

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,7 +7,7 @@ const Title = ({ className, width = "w-12 " }) => {
       className={`text-yellow text-2xl font-bold flex items-center gap-3 z-10 ${className}`}
     >
       <img src="/p2p-logo.png" className={`${width}`} alt="" />
-      CRYPTOP2P.PHP
+      CRYPTOP2P.PH
     </div>
   );
 };
